f05a1d8489d17ec695401cd447a2dd41d2a56f95
Ayush Morya <ayushmorya2480@gmail.com>
2025-11-12 20:43:42 +0530

    added functions feature

--- Hello.c
+++ Hello.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+void swap(int *, int *);
+
 int main() {
     int num1, num2;
     printf("Enter num1 : ");
@@ -7,16 +9,15 @@ int main() {
     printf("Enter num2 : ");
     scanf("%d", &num2);
 
-    int *x, *y;
-    x = &num1;
-    y = &num2;
+    swap(&num1, &num2);
+    printf("num1 = %d  num2 = %d", num1, num2);
+    
+    return 0;
+}
+
+void swap(int *x, int *y) {
     int t;
     t = *x;
     *x = *y;
     *y = t;
-
-    printf("num1 = %d \n", *x);
-    printf("num2 = %d \n", *y);
-    
-    return 0;
 }
